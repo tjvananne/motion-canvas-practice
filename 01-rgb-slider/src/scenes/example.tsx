7487df9b81f6d970ef29a7c3332f002464f5e89a
tjvananne <tjvananne@gmail.com>
2024-02-03 13:21:03 -0700

    Number signal to hex code function

--- 01-rgb-slider/src/scenes/example.tsx
+++ 01-rgb-slider/src/scenes/example.tsx
@@ -1,12 +1,13 @@
 import {Rect, Txt, makeScene2D} from '@motion-canvas/2d';
-import {all, createSignal, createRef} from '@motion-canvas/core';
+import {all, createSignal, createRef, SimpleSignal} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
 
   // At this point, I have three separate signals that each individually get converted
-  // into RGB hex code in the Rect.fill and Txt.text objects. I'd really like to separate
-  // out that logic for calculating those values into a separate function. I'm not sure how
-  // that would work though since those properties take in a lambda with no inputs?
+  // into RGB hex code in the Rect.fill and Txt.text objects. I've created a function
+  // that calculates the hex code for each signal integer to simplify the logic in
+  // the Rect.fill attribute. Next step is giving more thought to the overall Layout.
+  // Might need to go learn FlexBox.
 
   const sig_red = createSignal(255);
   const sig_green = createSignal(255);
@@ -14,25 +15,31 @@ export default makeScene2D(function* (view) {
   const rect = createRef<Rect>();
   const txt = createRef<Txt>();
 
+  function int_sig_to_hex(s: SimpleSignal<number, void>): string {
+    // Example: 168 --> "a8"
+    return Math.round(s()).toString(16).padStart(2, '0');
+  }
+
   view.add(
     <>
       <Rect
         ref={rect}
         width={300}
         height={300}
-        fill={() => `#${Math.round(sig_red()).toString(16).padStart(2, '0')}${Math.round(sig_green()).toString(16).padStart(2, '0')}${Math.round(sig_blue()).toString(16).padStart(2, '0')}`}
+        fill={() => `#${int_sig_to_hex(sig_red)}${int_sig_to_hex(sig_green)}${int_sig_to_hex(sig_blue)}`}
       />
       <Txt
         ref={txt}
-        text={() => `red: ${Math.round(sig_red()).toString(16).padStart(2, '0')}\ngreen: ${Math.round(sig_green()).toString(16).padStart(2, '0')}\nblue: ${Math.round(sig_blue()).toString(16).padStart(2, '0')}`}
+        // TODO: should probably be individual Txt objects once I start creating their own <Rect>...
+        text={() => `red: ${Math.round(sig_red())}\ngreen: ${Math.round(sig_green())}\nblue: ${Math.round(sig_blue())}`}
         y={250}
       />
     </>
   )
 
   yield* all(
-    sig_red(0, 4).to(255, 4),
-    sig_green(0, 6).to(255, 6),
-    sig_blue(0, 12).to(255, 12),
+    sig_red(0, 2).to(255, 2),
+    sig_green(0, 3).to(255, 3),
+    sig_blue(0, 4).to(255, 4),
   )
 });
